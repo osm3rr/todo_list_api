a9337faabc8bb9cb655919e0d4a813682cea7164
osm3rr <parabavireo@gmail.com>
2024-05-11 16:47:08 -0400

    model added

---
+++ todos/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Task
+
+# Register your models here.
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'body')
+    search_fields = ('title', 'body')
+    list_filter = ('title', 'body')
+    list_per_page = 10
+    ordering = ('title',)
+    
+    
+admin.site.register(Task, TaskAdmin)
